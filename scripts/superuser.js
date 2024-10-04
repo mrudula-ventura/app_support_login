def2a61125db265c57fcd98d23bf13ca5effe347
Intern Digital <interndig@vensec.com>
2024-10-04 11:22:36 +0530

    wallet css changes,superuser.js changed

--- scripts/superuser.js
+++ scripts/superuser.js
@@ -15,6 +15,35 @@ document.addEventListener('DOMContentLoaded', function() {
         return;
     }
 
+    /*getting clientid from email and mobile number*/
+
+    
+function searchClientId() {
+    const emailOrPhone = document.getElementById('email-mobile').value;
+    const clientIdDisplay = document.getElementById('client-id-display');
+
+    if (emailOrPhone) {
+        const clientId = clientDatabase[emailOrPhone];
+        if (clientId) {
+            clientIdDisplay.innerText = `Client ID: ${clientId}`;
+        } else {
+            clientIdDisplay.innerText = 'Client ID: ID not found';
+        }
+    } else {
+        alert('Please enter an email or phone number');
+    }
+}
+
+function submitClientId() {
+    const clientId = document.getElementById('client-id').value;
+    if (clientId) {
+        window.location.href = `client_page.html?clientId=${clientId}`;
+    } else {
+        alert('Please enter a Client ID');
+    }
+}
+
+
     
     submitButton.addEventListener('click', async function(event) {
         event.preventDefault();  
